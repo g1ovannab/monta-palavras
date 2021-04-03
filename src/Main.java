07120080b47ef808f2692b2e1e56eafd67904a69
Giovanna Bueno <giovanna.bueno.b@gmail.com>
2021-04-03 00:18:17 -0300

    Updated the Code. Added methods that filter the search for the words.

--- src/Main.java
+++ src/Main.java
@@ -69,7 +69,7 @@ public class Main{
 
             /* Chamada do método que acha as palavras que podem ser formadas a partir das
             letras digitadas pelo usuário. */
-            getWordsFound(typedLettersSorted, wordsArray, wordsFound, lettersNotUsed);
+            lettersNotUsed = getWordsFound(typedLettersSorted, wordsArray, wordsFound);
 
             /* CONTROLE. APAGAR DEPOIS. */
             System.out.println("Words found: ");
@@ -88,16 +88,92 @@ public class Main{
             /* A lista de palavras achadas tem um tamanho igual a 1? */
             else if (wordsFound.size() == 1){
                 /* Se sim, somente uma palavra foi achada. Iremos calcular quantos pontos essa
-                palavra fez a partir do método 'getPointsOfWords' e retornar para o usuário. */
-                byte points = getPointsOfWord(wordsFound.get(0));
-                System.out.println("\n" + wordsFound.get(0).toUpperCase() + ", palavra de " + points + " pontos");
+                palavra fez a partir do método 'getScoreOfWords' e retornar para o usuário. */
+                byte score = getScoreOfWord(wordsFound.get(0));
+                System.out.println("\n" + wordsFound.get(0).toUpperCase() + ", palavra de " + score + " pontos");
+
+                char[] lettersLeft = lettersNotUsed.get(0).toCharArray();
+                
+                System.out.println("\nSobraram: ");
+                for (char letter : lettersLeft){
+                    System.out.print(letter);
+                }
 
             } 
             /* A lista de palavras achadas tem um tamanho diferente de 0 e 1 (ou seja,
             mais de uma palavra foi achada)? */
             else {
-                /* Se sim, iremos pegar a palavra com mais pontos e retornar para o usuário. */
-                getWordWithMostPoints(wordsFound);
+                /* Se sim, iremos achar o index (baseado na lista de palavras achadas) da(s)
+                palavra(s) com maior pontuação. */
+                ArrayList<Byte> indexesOfWordsWithHighestScore = getWordsWithHighestScore(wordsFound);
+
+                /* Se só tiver um index, significa que só tem uma palavra com a maior pontuação,
+                então mostre ao usuátio. */
+                if(indexesOfWordsWithHighestScore.size() == 1){
+                    String theWord = wordsFound.get(indexesOfWordsWithHighestScore.get(0));
+                    byte score = getScoreOfWord(theWord);
+                    System.out.println("\n" + theWord.toUpperCase() + ", palavra de " + score + " pontos");
+
+                    char[] lettersLeft = lettersNotUsed.get(indexesOfWordsWithHighestScore.get(0)).toCharArray();
+                
+                    System.out.println("\nSobraram: ");
+                    for (char letter : lettersLeft){
+                        System.out.print(letter);
+                    }
+
+                } 
+                
+                /* Se não, significa que existem duas palavras com a mesma pontuação 
+                (a maior no caso).*/
+                else {
+                    /* Então iremos achar o index (baseado na lista de palavras achadas, e
+                    nas palavras com maior pontuação) das palavras com menor length. */
+                    ArrayList<Byte> indexesOfWordsWithLowestLength = getWordsWithLowestLength(wordsFound, indexesOfWordsWithHighestScore);
+                    
+                    /* Se só tiver um index, significa que só tem uma palavra com a menor
+                    length, então mostre ao usuário. */
+                    if(indexesOfWordsWithLowestLength.size() == 1){
+
+                        String theWord = wordsFound.get(indexesOfWordsWithLowestLength.get(0));
+                        byte score = getScoreOfWord(theWord);
+                        System.out.println("\n" + theWord.toUpperCase() + ", palavra de " + score + " pontos");
+
+                        char[] lettersLeft = lettersNotUsed.get(indexesOfWordsWithLowestLength.get(0)).toCharArray();
+                
+                        System.out.println("\nSobraram: ");
+                        for (char letter : lettersLeft){
+                            System.out.print(letter);
+                        }
+                    } 
+                    
+                    /* Se não, significa que existem duas palavras com a mesma length
+                    (a menor no caso). */
+                    else{
+                        /* Finalmente, baseando-se na lista de palavras achadas, palavras com
+                        maior pontuação e menor length, procuraremos a menor palavra 
+                        alfabeticamente falando. */
+                        ArrayList<String> indexesOfWords = getWordAlphabeticallySorted(wordsFound, indexesOfWordsWithLowestLength);
+                        
+                        String theWord = indexesOfWords.get(0);
+                        byte score = getScoreOfWord(theWord);
+                        
+                        System.out.println("\n" + theWord.toUpperCase() + ", palavra de " + score + " pontos");
+
+                        byte indexOfTheWord = 0;
+                        for (int i = 0; i < wordsFound.size(); i++){
+                            if (wordsFound.get(i).contains(theWord)) {
+                                indexOfTheWord = (byte) i;
+                            }
+                        }
+                        char[] lettersLeft = lettersNotUsed.get(indexOfTheWord).toCharArray();
+                
+                        System.out.println("\nSobraram: ");
+                        for (char letter : lettersLeft){
+                            System.out.print(letter);
+                        }
+                    }
+                }
+                
             }
 
 
@@ -107,10 +183,11 @@ public class Main{
 
 
             /* Código que controla se o usuário quer jogar novamente. */
-            System.out.println("\nVocê deseja jogar outra rodada?");
+            System.out.println("\n\nVocê deseja jogar outra rodada?");
             repeatAwnser = read.nextLine().toLowerCase();
 
-            if (repeatAwnser.equals("y") || repeatAwnser.equals("yes") || repeatAwnser.equals("s") || repeatAwnser.equals("sim")){
+            if (repeatAwnser.equals("y") || repeatAwnser.equals("yes") || 
+                repeatAwnser.equals("s") || repeatAwnser.equals("sim")){
                 repeat = true;
             } else{
                 repeat = false;
@@ -121,92 +198,220 @@ public class Main{
         read.close();
     }
 
-    public static void getWordWithMostPoints(ArrayList<String> wordsFound){
-        ArrayList<Byte> points = new ArrayList<>();
+    public static ArrayList<Byte> getWordsWithHighestScore(ArrayList<String> wordsFound){
+        
+        /* Lista que manterá os pontos das palavras achadas. */
+        ArrayList<Byte> scores = new ArrayList<>();
 
+        /* Para cada palavra achada, vamos adicionar a pontuação na lista. */
         for (byte i = 0; i < wordsFound.size(); i++){
-            points.set(i, getPointsOfWord(wordsFound.get(i)));
+            scores.add(getScoreOfWord(wordsFound.get(i)));
         }
 
-        if (points.size() == 1){
+        /* Lista que manterá os indexes da(s) maior(es) pontuação(ções). */
+        ArrayList<Byte> indexesOfHighestScores = new ArrayList<>();
+        /* Independente de ter duas palavras com a mesma pontuação, vamos pegar 
+        somente o valor da maior pontuação como referência. */
+        byte maxIndex = Collections.max(scores);
 
-        } else{
-            ArrayList<Byte> indexes = new ArrayList<>();
-            byte max = Collections.min(points);
-    
-            for (byte i = 0; i < points.size(); i++){
-                if (points.get(i) == max){
-                    
-                    indexes.add(i);
-                } else if (points.get(i) > max){
-                    
-                    indexes.clear();
-                    indexes.add(i);
-                    max = points.get(i);
-                }
+        /* Para cada pontuação (ou para cada palavra), vamos pegar todas
+        as pontuações máximas (todas no caso de ter duas máximas iguais): */
+        for (byte i = 0; i < scores.size(); i++){
+            /* Se a pontuação analisada for igual a pontuação máxima: */
+            if (scores.get(i) == maxIndex){
+                /* Adiciona-se o index da pontuação na lista de indexes. */
+                indexesOfHighestScores.add(i);
+            } 
+            /* Se a pontuação analisada for maior que a pontuação máxima:  */
+            else if (scores.get(i) > maxIndex){
+                /* Limparemos a lista de indexes, uma vez que os valores guardados nela
+                agora são menores que o máximo; */
+                indexesOfHighestScores.clear();
+                /* Adicionaremos o index da pontuação máxima atual. */
+                indexesOfHighestScores.add(i);
+                /* E por fim, atualiza-se o valor máximo da pontuação. */
+                maxIndex = scores.get(i);
             }
         }
 
+        return indexesOfHighestScores;
     } 
 
-    public static byte getPointsOfWord(String word){
-        byte points = 0;
+    public static ArrayList<Byte> getWordsWithLowestLength(ArrayList<String> wordsFound, ArrayList<Byte> indexesOfHighestScores){
+        
+        /* Lista que manterá as lengths das palavras com maior pontuação.*/
+        ArrayList<Byte> lengthsOfHighestScores = new ArrayList<>();
 
-        char[] wordInChars = word.toCharArray();
+        /* Para isso, precisamos procurar por essas lengths. */
+        for (byte i = 0; i < indexesOfHighestScores.size(); i++){
+            /* Para cada palavra com a maior pontuação, vamos adicionar
+            sua length à lista. */
+            byte lengthOfWord = (byte) (wordsFound.get(indexesOfHighestScores.get(i))).length();
+            lengthsOfHighestScores.add(lengthOfWord);
+        }
 
-        for (char c : wordInChars){
-            if (c == 'a' || c == 'e' || c == 'i' || c == 'l' || c == 'n' || 
-            c == 'o' || c == 'r' || c == 's' || c == 't' || c == 'u'){
-                points+=1;
-            } else if (c == 'd' || c == 'g'){
-                points+=2;
-            } else if (c == 'b' || c == 'c' || c == 'm' || c == 'p'){
-                points+=3;
-            } else if (c == 'f' || c == 'h' || c == 'v'){
-                points+=5;
-            } else if (c == 'j' || c == 'x'){
-                points+=8;
-            } else if (c == 'q' || c == 'z'){
-                points+=13;
+        /* Lista que manterá os indexes da(s) palavra(s) com menor(es) length. */
+        ArrayList<Byte> indexesOfLowestLenghts = new ArrayList<>();
+
+        /* Independente de ter duas palavras com a mesma length, vamos pegar 
+        somente o valor da menor length como referência.  */
+        byte minLength = Collections.min(lengthsOfHighestScores);
+        
+        /* Aqui, analizaremos os indexes das palavras com maiores pontuações. */
+        for (byte i = 0; i < indexesOfHighestScores.size(); i++){
+
+            /* Se a length da palavra analisada (que tem a maior pontuação)
+            for igual a length mínima: */
+            if (lengthsOfHighestScores.get(i) == minLength){
+                /* Adiciona-se o index da palavra de maior pontuação, na lista de indexes
+                de palavras com menores lengths. */
+                indexesOfLowestLenghts.add(indexesOfHighestScores.get(i));
+            } 
+            /* Se a lenght da palavra analisada (que tem a maior pontuação)
+            for menor que a length mínima: */
+            else if (lengthsOfHighestScores.get(i) < minLength){
+                /* Limparemos a lista de indexes com menores lengths, uma vez que
+                os valores guardados nela agora são maiores que o mínimo. */
+                indexesOfLowestLenghts.clear();
+                /* Adicionaremos o index da maior pontuação atual. */
+                indexesOfLowestLenghts.add(indexesOfHighestScores.get(i));
+                /* E por fim, atualiza-se o valor mínimo da length. */
+                minLength = lengthsOfHighestScores.get(i);
             }
         }
 
-        return points;
+        return indexesOfLowestLenghts;
     }
 
-    public static void getWordsFound(String typedLetters, ArrayList<String> words, 
-    ArrayList<String> wordsFound, ArrayList<String> lettersNotUsed){
-        // ArrayList<String> wordsFound = new ArrayList<>();
-        //make a copy to work on
-
-        ArrayList<String> copyOfAllWords = new ArrayList<String>(words);
-        // Collections.copy(copy, words);
+    public static ArrayList<String> getWordAlphabeticallySorted(ArrayList<String> wordsFound, ArrayList<Byte> indexesOfLowestLenghts){
         
+        /* Lista que manterá as palavras com maior pontuação e menor length. */
+        ArrayList<String> wordsWithHighestScoreAndLowestLenght = new ArrayList<>();
+
+        /* Para isso, precisamos percorrer a lista de indexes de palavras com
+        menores lengths (e consequentemente maiores pontuações). */
+        for (byte i = 0; i < indexesOfLowestLenghts.size(); i++){
+            /* Para cada index, pegaremos a palavra correspondente e adicionaremos
+            a lista. */
+            String word = wordsFound.get(indexesOfLowestLenghts.get(i));
+            wordsWithHighestScoreAndLowestLenght.add(word);
+        }
+
+        /* Agora temos uma lista filtrada com as palavras achadas pela pesquisa
+        com a maior pontuação possível e menor length possível. Entretanto, se existem 
+        mais de uma palavra nessa lista, as palavras tem mesma pontuação e mesmo length, 
+        e para desempatar precisamos ver qual delas vem primeiro, alfabeticamente falando.
+        Utiliza-se o método 'sort' para ordenar as palavras. */
+        Collections.sort(wordsWithHighestScoreAndLowestLenght);
+
+        /* Finalmente, retorna-se a palavra com maior pontuação, menor length, e 
+        alfabeticamente ordenada. */
+        return wordsWithHighestScoreAndLowestLenght;
+    }
+
+    public static byte getScoreOfWord(String word){
+        byte score = 0;
+
+        /* Transforma a palavra em um array de chars. */
+        char[] wordInChars = word.toCharArray();
+
+        /* Para cada char no array de chars, faça: */
+        for (char c : wordInChars){
+            /* Se o char for igual a uma das letras listadas na condição, a contagem de pontos soma 1. */
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'l' || c == 'n' || 
+            c == 'o' || c == 'r' || c == 's' || c == 't' || c == 'u'){
+                score+=1;
+            } else if (c == 'd' || c == 'g'){ /* Soma 2. */
+                score+=2;
+            } else if (c == 'b' || c == 'c' || c == 'm' || c == 'p'){ /* Soma 3. */
+                score+=3;
+            } else if (c == 'f' || c == 'h' || c == 'v'){ /* Soma 5. */
+                score+=5;
+            } else if (c == 'j' || c == 'x'){ /* Soma 8. */
+                score+=8;
+            } else if (c == 'q' || c == 'z'){ /* Soma 13. */
+                score+=13;
+            }
+        }
+
+        return score;
+    }
+
+    public static ArrayList<String> getWordsFound(String typedLetters, ArrayList<String> words, 
+    ArrayList<String> wordsFound){
+        
+        /* Lista que manterá as letras não usadas de uma palavra. */
+        ArrayList<String> lettersNotUsed = new ArrayList<>();
+
+        /* Cópia da lista que contém todas as palavras. */
+        ArrayList<String> copyOfAllWords = new ArrayList<String>(words);
+
+        /* Tranformando as letras digitadas pelo usuário em um array de chars. */
         char[] lettersInChars = typedLetters.toCharArray();
 
+        /* Para cada letra no array de letras digitadas pelo usuário, faça: */
         for(char letter : lettersInChars) {
+            /* Loop que percorre a cópia da lista de palavras. */
             for (int i = 0; i < copyOfAllWords.size(); i++) {
-
+                /* Ordenando a palavra que está sendo analisada. */
                 String wordSorted = getSortedString(copyOfAllWords.get(i));
 
+                /* Se essa palavra não estiver em branco (somente estaços e não letras), ou vazia:  */
                 if(!wordSorted.isBlank()){
+                    /* Troca na palavra (que está na posição i da cópia da lista de palavras),
+                    o primeiro char que é igual a letra que estamos analisando, por um espaço vazio.
+                    Ou seja, significa que o char digitado pelo usuário foi achado na palavra. */
                     copyOfAllWords.set(i, wordSorted.replaceFirst(Character.toString(letter), ""));
                 }
             }
         }
 
+        /* Finalmente, agora procuraremos as palavras achadas. */
         for (int i = 0; i < copyOfAllWords.size(); i++){
+            /* Variável que manterá todas as letras não utilizadas para formar a palavra. */
+            String lettersNotUsedString = "";
+
+            /* Se na cópia da lista de palavras, a primeira posição estiver vazia, 
+            significa que todas as letras dessa palavra foram achadas nas letras 
+            digitadas pelo usuário. */
             if(copyOfAllWords.get(i).isBlank()){
+
+                /* Por isso, vamos adicionar essa palavra à lista de palavras achadas. */
                 wordsFound.add(words.get(i));
-            }     //find empty strings
+
+                /* Entretanto, para achar as letras não utilzadas, percorreremos o array
+                de de letras digitadas pelo usuário. */
+                for (char letter : lettersInChars){
+                    /* Se a palavra na posição indicada
+                    !ATENÇÃO! Utilizamos essa palavra (da lista geral de palavras) pois entramos no if 
+                    !condicional (ou seja, essa é uma palavra achada).
+                    não tiver a letra analisada (isso por que o método 'indexOf' indica 
+                    o index na qual essa letra se encontra. Se o index for -1, significa 
+                    que a letra nãso consta na String), essa letra não foi utilizada.*/
+                    if (words.get(i).indexOf(letter) == -1){
+                        /* Concatena-se a letra não utilizada na String de letras não utilizadas
+                        dessa palavra. */
+                        lettersNotUsedString += letter;
+                    }
+                }
+
+                /* Por fim, antes de percorrer a próxima palavra, adiciona-se a String que mantém
+                as letras não utilizadas naquela palavra, a uma lista. */
+                lettersNotUsed.add(lettersNotUsedString);
+            }
+            
         }
 
+        return lettersNotUsed;
     }
 
-    public static String getSortedString(String stringToSort){
-        char[] lettersArray = stringToSort.toCharArray();
-        Arrays.sort(lettersArray);
-        String sortedString = new String(lettersArray);
+    public static String getSortedString(String word){
+        /* Transformando a palavra em um array de chars. */
+        char[] wordInChars = word.toCharArray();
+        /* Utiliza-se o método 'sort' da classe Arrays que automaticamente 
+        ordena as letras alfabeticamente. */
+        Arrays.sort(wordInChars);
+        String sortedString = new String(wordInChars);
         return sortedString;
     }
 
